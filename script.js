c5f01f0b9aef80bccc170fd245369faa2b07d73e
Mugambi Lewis <mugambilewis001@gmail.com>
2024-11-23 20:01:43 +0300

    full HTML

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 function opentab(tabname){
@@ -10,3 +13,42 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+
+const words = ["Web Developer", "Web Designer", "Graphics Designer", "CAD Designer"];
+let currentWordIndex = 0;
+let currentLetterIndex = 0;
+const typingSpeed = 100;
+const erasingSpeed = 50;
+const delayBetweenWords = 1000;
+
+const typedTextSpan = document.getElementById("typed-text");
+
+function type() {
+  if (currentLetterIndex < words[currentWordIndex].length) {
+    typedTextSpan.textContent += words[currentWordIndex].charAt(currentLetterIndex);
+    currentLetterIndex++;
+    setTimeout(type, typingSpeed);
+  } else {
+    setTimeout(erase, delayBetweenWords);
+  }
+}
+
+function erase() {
+  if (currentLetterIndex > 0) {
+    typedTextSpan.textContent = words[currentWordIndex].substring(0, currentLetterIndex - 1);
+    currentLetterIndex--;
+    setTimeout(erase, erasingSpeed);
+  } else {
+    currentWordIndex = (currentWordIndex + 1) % words.length;
+    setTimeout(type, typingSpeed);
+  }
+}
+
+// Start the typing animation
+document.addEventListener("DOMContentLoaded", function() {
+  setTimeout(type, delayBetweenWords);
+});
+
+c
+ 
